62a6ef09bde4a3b522465537eb1e2d16609345f1
qqweweqewe <golubkovivan4@gmail.com>
2026-04-01 15:36:06 +0300

    first commit

---
+++ qwew-backend/src/main.rs
@@ -0,0 +1,68 @@
+use axum::{
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::Response,
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod config;
+mod db;
+mod handlers;
+mod models;
+mod routes;
+mod ws;
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    tracing::info!("==! starting Qwew backend !==");
+
+    dotenvy::dotenv().ok();
+
+    let app = Router::new()
+        .route("/", get(|| async { "Qwew backend is running" }))
+        .route("/ws", get(ws_handler))
+        .layer(CorsLayer::permissive())
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::info!("listening on http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    tracing::info!("new WebSocket client connected");
+
+    while let Some(msg) = socket.recv().await {
+        match msg {
+            Ok(msg) => {
+                if let Err(e) = socket.send(msg).await {
+                    tracing::error!("WebSocket send error: {}", e);
+                    break;
+                }
+            }
+            Err(e) => {
+                tracing::error!("WebSocket error: {}", e);
+                break;
+            }
+        }
+    }
+
+    tracing::info!("WebSocket client disconnected");
+}
